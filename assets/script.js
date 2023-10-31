5b23ad6c4574c59f61a23e60779510ee59ce8658
rodrigoaoliva <rodrigoaoliva1@gmail.com>
2023-10-30 22:15:18 -0400

    declared variables in javascript, modified elements id's to match the variables and linked JS file to html

--- assets/script.js
+++ assets/script.js
@@ -50,3 +50,27 @@ const questions = [
         correctAnswer: 3
     },
 ];
+
+let currentQuestionIndex = 0;
+        let timer = 60;
+        let correctAnswers = 0;
+        let totalQuestions = questions.length;
+
+        const timerElement = document.getElementById("timer");
+        const questionText = document.getElementById("question-text");
+        const options = document.getElementById("options");
+        const wrongAnswer = document.getElementById("wrong-answer");
+        const viewScores = document.getElementById("view-scores");
+
+        const beginQuizSection = document.querySelector(".begin-quiz");
+        const startQuizButton = document.getElementById("start-quiz");
+        const questionsSection = document.querySelector(".questions");
+        const scoreSection = document.getElementById("game-over");
+        const initialsInput = document.getElementById("initials");
+        const submitScoreButton = document.getElementById("submit-score");
+        const highScoresSection = document.querySelector(".high-scores");
+        const highScoresList = document.getElementById("high-scores-list");
+        const goBackButton = document.getElementById("go-back");
+        const clearScoresButton = document.getElementById("clear-scores");
+
+        let highScores = []; 
