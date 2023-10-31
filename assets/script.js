e8f3667b58906decc1d9c60301de324ba29f7c07
rodrigoaoliva <rodrigoaoliva1@gmail.com>
2023-10-30 22:06:52 -0400

    added multiple choice questions to the javascript

---
+++ assets/script.js
@@ -0,0 +1,52 @@
+const questions = [
+    {
+        question: "Commonly used data types DO Not include:",
+        answers: [
+            "Strings",
+            "Booleans",
+            "Alerts",
+            "Numbers"
+        ],
+        correctAnswer: 2
+    },
+    {
+        question: "The condition in an if / else statement is enclosed with _______",
+        answers: [
+            "Quotes",
+            "Curly brackets",
+            "Parenthesis",
+            "Square brackets"
+        ],
+        correctAnswer: 0
+    },
+    {
+        question: "Arrays in JavaScript can be used to store _______",
+        answers: [
+            "Numbers and strings",
+            "Other arrays",
+            "Booleans",
+            "All of the above"
+        ],
+        correctAnswer: 2
+    },
+    {
+        question: "String values must be enclosed within _______ when being assigned to variables.",
+        answers: [
+            "Commas",
+            "Curly brackets",
+            "Quotes",
+            "Parentheses"
+        ],
+        correctAnswer: 2
+    },
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        answers: [
+            "JavaScript",
+            "Terminal/bash",
+            "For loops",
+            "console.log"
+        ],
+        correctAnswer: 3
+    },
+];
