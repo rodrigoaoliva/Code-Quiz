dce8701de9f4999af0a24ef9aad97728bf3459f7
rodrigoaoliva <rodrigoaoliva1@gmail.com>
2023-10-30 22:28:11 -0400

    functions to check if answer is correct, display the text with a content saying if the choice is right or wrong, deduct time if answer is wrong, end the game if time is less than 0, and increase the index by one after an answer is chosen

--- assets/script.js
+++ assets/script.js
@@ -52,39 +52,39 @@ const questions = [
 ];
 
 let currentQuestionIndex = 0;
-        let timer = 60;
-        let correctAnswers = 0;
-        let totalQuestions = questions.length;
+let timer = 60;
+let correctAnswers = 0;
+let totalQuestions = questions.length;
 
-        const timerElement = document.getElementById("timer");
-        const questionText = document.getElementById("question-text");
-        const options = document.getElementById("options");
-        const wrongAnswer = document.getElementById("wrong-answer");
-        const viewScores = document.getElementById("view-scores");
+const timerElement = document.getElementById("timer");
+const questionText = document.getElementById("question-text");
+const options = document.getElementById("options");
+const wrongAnswer = document.getElementById("wrong-answer");
+const viewScores = document.getElementById("view-scores");
 
-        const beginQuizSection = document.querySelector(".begin-quiz");
-        const startQuizButton = document.getElementById("start-quiz");
-        const questionsSection = document.querySelector(".questions");
-        const scoreSection = document.getElementById("game-over");
-        const initialsInput = document.getElementById("initials");
-        const submitScoreButton = document.getElementById("submit-score");
-        const highScoresSection = document.querySelector(".high-scores");
-        const highScoresList = document.getElementById("high-scores-list");
-        const goBackButton = document.getElementById("go-back");
-        const clearScoresButton = document.getElementById("clear-scores");
+const beginQuizSection = document.querySelector(".begin-quiz");
+const startQuizButton = document.getElementById("start-quiz");
+const questionsSection = document.querySelector(".questions");
+const scoreSection = document.getElementById("game-over");
+const initialsInput = document.getElementById("initials");
+const submitScoreButton = document.getElementById("submit-score");
+const highScoresSection = document.querySelector(".high-scores");
+const highScoresList = document.getElementById("high-scores-list");
+const goBackButton = document.getElementById("go-back");
+const clearScoresButton = document.getElementById("clear-scores");
 
-        let highScores = []; 
+let highScores = []; 
 
-        startQuizButton.addEventListener("click", startQuiz);
+startQuizButton.addEventListener("click", startQuiz);
 
         function startQuiz() {
             beginQuizSection.style.display = "none"; // Hide the "begin-quiz" section
             questionsSection.style.display = "block";
             startTimer();
             showQuestion(currentQuestionIndex);
-        }
+}
 
-        function showQuestion(questionIndex) {
+function showQuestion(questionIndex) {
             const question = questions[questionIndex];
             questionText.textContent = question.question;
 
@@ -98,4 +98,28 @@ let currentQuestionIndex = 0;
                 button.addEventListener("click", handleAnswerClick);
                 options.appendChild(button);
             });
-        }
+}
+
+function handleAnswerClick(event) {
+            const selectedAnswerIndex = parseInt(event.target.dataset.index);
+            if (selectedAnswerIndex === questions[currentQuestionIndex].correctAnswer) {
+                correctAnswers++;
+                wrongAnswer.style.display = "block";
+                wrongAnswer.textContent = "Right!";
+            } else {
+                timer -= 10; // Deduct 10 seconds for an incorrect answer
+                wrongAnswer.style.display = "block";
+                wrongAnswer.textContent = "Wrong!";
+                if (timer < 0) {
+                    timer = 0;
+                }
+            }
+
+            currentQuestionIndex++;
+            if (currentQuestionIndex < totalQuestions) {
+                showQuestion(currentQuestionIndex);
+            } else {
+                finalTimerValue = timer;
+                endGame();
+            }
+}
